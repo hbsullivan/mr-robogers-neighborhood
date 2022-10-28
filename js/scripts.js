567cec2f95c725b36c9863698187820a33728ec4
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-28 11:27:34 -0700

    WIP: test to convert all numbers containing a 3 into wont you be my neighbor

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,20 @@ function beepBoop(countUpArray) {
   let stringArray = countUpArray.map(function(element){
     return element.toString();
   });
-  console.log(stringArray)
-  for (let i = 1; i <= stringArray.length; i+=1) {
-    if (stringArray[i] === "3") {
-      stringArray[i] = "won't you be my neighbor?";
-    } else if (stringArray[i] === "2") {
-      stringArray[i] = "Boop!";
-    } else if (stringArray[i] === "1") {
-      stringArray[i] = "Beep!"
-    }
-  }
+  console.log(stringArray);
   
-  return stringArray
-}
+  
+
+//  for (let i = 1; i <= stringArray.length; i+=1) {
+
+//    if (stringArray[i].includes("3")) {
+//       stringArray[i] = "won't you be my neighbor?";
+//     } else if (stringArray[i] === "2") {
+//       stringArray[i] = "Boop!";
+//     } else if (stringArray[i] === "1") {
+//        stringArray[i] = "Beep!"
+//     }
+//   }
+
+//    return stringArray
+ }
