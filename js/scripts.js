1f940e1c5448b91a1555a48ace9c8b8985381c5a
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-28 09:46:45 -0700

    test to replace 2s with boop! passes

--- js/scripts.js
+++ js/scripts.js
@@ -11,8 +11,8 @@ function beepBoop(countUpArray) {
   for (let i = 1; i <= countUpArray.length; i+=1) {
     if (countUpArray[i] === 3) {
       countUpArray[i] = "won't you be my neighbor?";
-    // } else if (countingUpArray[i] === 2) {
-    //   countUpArray[i] = "Boop!";
+    } else if (countUpArray[i] === 2) {
+      countUpArray[i] = "Boop!";
     }
   }
   console.log(countUpArray)
