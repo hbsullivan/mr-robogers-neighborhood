28f09c1c12d2aa3c74252d80140b199879769f08
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-28 14:21:20 -0700

    test passed where other numbers containing 3 are changed to desired text

--- js/scripts.js
+++ js/scripts.js
@@ -15,7 +15,7 @@ function beepBoop(countUpArray) {
   });
   console.log(stringArray)
 
- for (let i = 1; i <= stringArray.length; i+=1) {
+ for (let i = 0; i < stringArray.length; i+=1) {
   // if (stringArray[i].length > 1) {
   //   for (let j = 1; i <= stringArray[j].length; j+=1) {
   //     if (stringArray[j] === "3") {
@@ -25,8 +25,24 @@ function beepBoop(countUpArray) {
   //   }
   //   console.log(stringArray);
   // }
+  firstNumber = stringArray[i].charAt(0);
+  secondNumber = stringArray[i].charAt(1);
+  thirdNumber = stringArray[i].charAt(3);
 
-   if (stringArray[i] === "3") {
+  if (firstNumber === "3" || secondNumber === "3" || thirdNumber === "3") {
+     stringArray[i] = "Won't you be my neighbor?";
+  // } else if (firstNumber === "2" || secondNumber === "2" ||  thirdNumber === "2") {
+  //    stringArray[i] = "Boop!";
+  // } else if (firstNumber === "1" || secondNumber === "1" || thirdNumber === "1") {
+  //     stringArray[i] = "Beep!"
+  // }
+  
+  
+  
+  
+  
+  firstNumber = stringArray[i].charAt()
+    if (stringArray[i] === "3") {
       stringArray[i] = "Won't you be my neighbor?";
     } else if (stringArray[i] === "2") {
       stringArray[i] = "Boop!";
