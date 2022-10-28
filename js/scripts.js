10ba75db29d9d5bcfea1e85b454d19df22ac0b09
Henry Sullivan <sullivanbhenry@gmail.com>
2022-10-28 14:45:24 -0700

    test passed to convert numbers with 2 or 1 into desired text

--- js/scripts.js
+++ js/scripts.js
@@ -13,42 +13,21 @@ function beepBoop(countUpArray) {
     return element.toString();
 
   });
-  console.log(stringArray)
 
- for (let i = 0; i < stringArray.length; i+=1) {
-  // if (stringArray[i].length > 1) {
-  //   for (let j = 1; i <= stringArray[j].length; j+=1) {
-  //     if (stringArray[j] === "3") {
-  //       stringArray[i] = "won't you be my neighbor?";
-        
-  //     }
-  //   }
-  //   console.log(stringArray);
-  // }
+  for (let i = 0; i < stringArray.length; i+=1) {
+
   firstNumber = stringArray[i].charAt(0);
   secondNumber = stringArray[i].charAt(1);
   thirdNumber = stringArray[i].charAt(3);
+  fourthNumber = stringArray[i].charAt(4);
 
-  if (firstNumber === "3" || secondNumber === "3" || thirdNumber === "3") {
-     stringArray[i] = "Won't you be my neighbor?";
-  // } else if (firstNumber === "2" || secondNumber === "2" ||  thirdNumber === "2") {
-  //    stringArray[i] = "Boop!";
-  // } else if (firstNumber === "1" || secondNumber === "1" || thirdNumber === "1") {
-  //     stringArray[i] = "Beep!"
-  // }
-  
-  
-  
-  
-  
-  firstNumber = stringArray[i].charAt()
-    if (stringArray[i] === "3") {
-      stringArray[i] = "Won't you be my neighbor?";
-    } else if (stringArray[i] === "2") {
-      stringArray[i] = "Boop!";
-    } else if (stringArray[i] === "1") {
-       stringArray[i] = "Beep!"
-    }
+  if (firstNumber === "3" || secondNumber === "3" || thirdNumber === "3" || fourthNumber === "3") {
+     stringArray[i] = " Won't you be my neighbor? ";
+  } else if (firstNumber === "2" || secondNumber === "2" ||  thirdNumber === "2" || fourthNumber === "2") {
+     stringArray[i] = " Boop! ";
+  } else if (firstNumber === "1" || secondNumber === "1" || thirdNumber === "1" || fourthNumber === "1") {
+      stringArray[i] = " Beep! "
+  }
   
   }
   return stringArray
