e36162ce68e9b69942ae23137912403e60ce1a31
Henry Sullivan <sullivanbhenry@gmail.com>
2022-11-01 17:42:24 -0700

    corects bug so that all numbers pass the test and refactored the code

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 //Business Logic
 
 function countUp(input) {
-  countUpArray = [];
+  let countUpArray = [];
   for(let i = input; i >= 0; i --) {
     countUpArray.push(i);
   }
@@ -16,46 +16,27 @@ function beepBoop(countUpArray) {
   });
 
   for (let i = 0; i < stringArray.length; i+=1) {
-    // for (let j = 0; j <= stringArray[i].length; i+=1) {
-      // let someNumber = stringArray[i].charAt[j]
-      if (i.includes("3")) {
+      if (stringArray[i].includes("3")) {
         stringArray[i] = " Won't you be my neighbor? ";
-      } else if (someNumber === "2") {
+      } else if (stringArray[i].includes("2")) {
         stringArray[i] = " Boop! ";
-      } else if (someNumber === "1") {
+      } else if (stringArray[i].includes("1")) {
         stringArray[i] = " Beep! ";
       }
-    // }
   }
-
-    firstNumber = stringArray[i].charAt(0);
-    secondNumber = stringArray[i].charAt(1);
-    thirdNumber = stringArray[i].charAt(3);
-    fourthNumber = stringArray[i].charAt(4);
-
-    if (firstNumber === "3" || secondNumber === "3" || thirdNumber === "3" || fourthNumber === "3") {
-     stringArray[i] = " Won't you be my neighbor? ";
-    } else if (firstNumber === "2" || secondNumber === "2" ||  thirdNumber === "2" || fourthNumber === "2") {
-     stringArray[i] = " Boop! ";
-    } else if (firstNumber === "1" || secondNumber === "1" || thirdNumber === "1" || fourthNumber === "1") {
-      stringArray[i] = " Beep! "
-    }
-  
+    return stringArray
   }
   
-  return stringArray
   
- }
-
 
  //UI Logic
 
  function gatherInput(event) {
   event.preventDefault();
   const userInput = document.getElementById("numberInput").value
-  countUp(userInput)
+  let newInput = countUp(userInput)
 
-  document.getElementById("return").innerText = beepBoop(countUpArray);
+  document.getElementById("return").innerText = beepBoop(newInput);
  }
 
  window.addEventListener("load", function() {
